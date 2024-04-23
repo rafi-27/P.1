1a67de4332d4320d77b4dfb5eba4adc650e78427
rafik <rafikbachri21@gmail.com>
2024-04-23 12:25:35 +0200

    23

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module proyectouno {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens proyectouno to javafx.fxml;
     exports proyectouno;
---
+++ src/main/java/proyectouno/primerPControl.java
@@ -0,0 +1,61 @@
+package proyectouno;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class primerPControl implements Initializable{
+    //Contenedores:
+    @FXML Pane panelPrincipal;
+    @FXML Pane panelCambiaColores;
+
+    //Sliders y textField
+    @FXML Slider R;
+    @FXML Slider G;
+    @FXML Slider B;
+
+    @FXML TextField RUno;
+    @FXML TextField GUno;
+    @FXML TextField BUno;
+
+    @FXML Label muestraValoresRGB;
+
+    @FXML Pane someButton;
+
+    @FXML
+    public void makeButtonWhite(MouseEvent event)
+    {
+        someButton.setStyle("-fx-background-color: #ffffff");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Color color = new Color(0, 0, 0, 0);
+        String enteredByUser = "abcdef";
+        panelCambiaColores.setStyle("-fx-background-color: #ffffff");
+
+        String primeHex = Double.toHexString(R.getValue());
+        String segundoHex = Double.toHexString(G.getValue());
+        String tercerHex = Double.toHexString(B.getValue());
+
+        RUno.setText(primeHex);
+        GUno.setText(segundoHex);
+        BUno.setText(tercerHex);
+
+        muestraValoresRGB.setText(primeHex+segundoHex+tercerHex);
+    }
+
+
+
+
+
+
+}
