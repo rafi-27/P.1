6a0ef36ceab30b4cb1a70a3f5a2072d47d5916a6
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-05-05 21:38:53 +0200

    ay

--- src/main/java/proyectouno/ControladorEjemplo.java
+++ src/main/java/proyectouno/ControladorEjemplo.java
@@ -7,20 +7,30 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
-public class ControladorEjemplo implements Initializable{
-    @FXML TextField mostrarNumeritos;
-    @FXML Slider sliderPa;
+public class ControladorEjemplo implements Initializable {
+    @FXML
+    TextField mostrarNumeritos;
+    @FXML
+    Slider sliderPa;
+
     @Override
-    
-    
+
     public void initialize(URL location, ResourceBundle resources) {
         sliderPa.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldTexto, Number newTexto) {
-                mostrarNumeritos.setText(String.format("%.0f",newTexto));
+                mostrarNumeritos.setText(String.format("%.0f", newTexto));
+            }
+        });
+        mostrarNumeritos.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldTexto, String newtexto) {
+                sliderPa.setValue(Double.parseDouble(newtexto));
             }
         });
     }
